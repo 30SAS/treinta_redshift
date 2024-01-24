1b1f13cda4aff5cde2cd71890cd420d2812dc15a
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-01-24 18:48:51 -0500

    modificamos estructura

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
     license='LICENSE',
-    description='Librería que permite programar sobre redshift utilizando python para tareas de data engineering',
-    long_description=open('README.md').read(),
+    description='Libreria para data engineering',
+    long_description='Librería que permite programar sobre redshift utilizando python para tareas de data engineering',
     install_requires=read_requirements(),  # Usar la función aquí
 )
