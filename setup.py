02d1e14bb72a9b23852cfd4dbfe645341fc9c254
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-01-24 19:01:17 -0500

    publish

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup, find_packages
 
 # Función para leer el contenido de requirements.txt
-def read_requirements():
-    with open('requirements.txt') as req:
-        return req.read().splitlines()
-
 setup(
     name='treinta_redshift',
     version='0.1.0',
@@ -14,5 +10,11 @@ setup(
     license='LICENSE',
     description='Libreria para data engineering',
     long_description='Librería que permite programar sobre redshift utilizando python para tareas de data engineering',
-    install_requires=read_requirements(),  # Usar la función aquí
+    install_requires=[
+        'boto3==1.34.23',
+        'botocore==1.34.23',
+        'numpy==1.25.0',
+        'pandas==2.0.2',
+        'pyarrow==13.0.0',
+    ]
 )
