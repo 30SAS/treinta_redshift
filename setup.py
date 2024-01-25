e96e7a49b411f71045678fb061bc55f1f5392cc7
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-01-24 19:30:06 -0500

    nueva version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treinta_redshift',
-    version='0.1.0',
+    version='0.1.1',
     author='cristian.pinela',
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
