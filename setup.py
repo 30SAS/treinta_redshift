621eaa0a7ea5cc21cbc3eb3326ad3ef1343767d4
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-01-24 22:14:02 -0500

    modificamos setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='treinta_redshift',
-    version='0.1.2',
+    version='0.1.3',  # Incremento de la versión
     author='cristian.pinela',
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
     license='LICENSE',
     description='Libreria para data engineering',
-    long_description='Librería que permite programar sobre redshift utilizando python para tareas de data engineering',
-    long_description_content_type='text/markdown',  # Specify Markdown format
+    long_description=open('README.md').read(),  # Asegurarse de que README.md no tiene errores de sintaxis
+    long_description_content_type='text/markdown',  # Especifica que la descripción larga está en Markdown
     install_requires=[
         'boto3==1.34.23',
         'botocore==1.34.23',
