b5d0bb43f130f05bfaced0904c9ef007988ec7dd
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-03-18 15:00:34 -0500

    añadimos modificación sobre región

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treinta_redshift',
-    version='1.0.7',  # Incremento de la versión
+    version='1.0.8',  # Incremento de la versión
     author='cristian.pinela',
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
