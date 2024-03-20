16a1400052f298b5fa31ccad14d5c2ac3c2a4615
cpinelatreinta <145484001+cpinelatreinta@users.noreply.github.com>
2024-03-20 11:00:57 -0500

    solution a boolean

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treinta_redshift',
-    version='1.0.9',  # Incremento de la versión
+    version='1.1',  # Incremento de la versión
     author='cristian.pinela',
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
