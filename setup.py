d8ac7ae58360b1731dd7dde0bc4f38dc86d8cb47
Juliantreinta <julian.oliveros@treinta.co>
2024-03-27 16:51:57 -0500

    fix return status

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='treinta_redshift',
-    version='1.1',  # Incremento de la versión
+    version='1.2',  # Incremento de la versión
     author='cristian.pinela',
     author_email='cristian.pinela@treinta.co',
     packages=find_packages(),
     license='LICENSE',
     description='paquete que permite interactuar pandas con redshift',
-    long_description=open('README.md').read(),  # Asegurarse de que README.md no tiene errores de sintaxis
-    long_description_content_type='text/markdown',  # Especifica que la descripción larga está en Markdown
+    # Asegurarse de que README.md no tiene errores de sintaxis
+    long_description=open('README.md').read(),
+    # Especifica que la descripción larga está en Markdown
+    long_description_content_type='text/markdown',
     install_requires=[
         'boto3',
         'botocore',
--- treinta_redshift/core.py
+++ treinta_redshift/core.py
@@ -7,10 +7,11 @@ import datetime
 import uuid
 from pandas import DataFrame
 
+
 def table_to_dataframe(table, schema, database='landing_zone', NUM_ENTRIES=0, cluster_identifier='redshift-data', region_name='us-west-2', db_user='admintreinta'):
     """
     Ejecuta una consulta SQL en Amazon Redshift para extraer datos de una tabla específica y devuelve los resultados como un DataFrame de pandas.
-    
+
     Parámetros:
     - table : Nombre de la tabla a consultar.
     - schema: Esquema de la base de datos donde se encuentra la tabla.
@@ -18,7 +19,7 @@ def table_to_dataframe(table, schema, database='landing_zone', NUM_ENTRIES=0, cl
     - NUM_ENTRIES: Número máximo de entradas a retornar. Si es 0, retorna todas las entradas.
     - cluster_identifier: Identificador del clúster de Amazon Redshift.
     - db_user: Usuario de la base de datos para ejecutar la consulta.
-    
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
@@ -26,7 +27,7 @@ def table_to_dataframe(table, schema, database='landing_zone', NUM_ENTRIES=0, cl
     sql_query = f"SELECT * FROM {schema}.{table} "
     if NUM_ENTRIES > 0:
         sql_query += f"LIMIT {NUM_ENTRIES}"
-        
+
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
         Database=database,
@@ -35,22 +36,23 @@ def table_to_dataframe(table, schema, database='landing_zone', NUM_ENTRIES=0, cl
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
-    
+
     if status == 'FINISHED':
         response = client.get_statement_result(Id=statement_id)
-        
+
         # Extrayendo los nombres de las columnas de la metadata de columnas
         column_metadata = response['ColumnMetadata']
         column_names = [column['name'] for column in column_metadata]
-        
+
         # Construyendo el DataFrame
         records = response['Records']
         df_rows = []
@@ -68,39 +70,39 @@ def table_to_dataframe(table, schema, database='landing_zone', NUM_ENTRIES=0, cl
                 elif 'booleanValue' in field:
                     row.append(field['booleanValue'])
                 else:
-                    row.append(None)  # Añadir soporte para más tipos según sea necesario
+                    # Añadir soporte para más tipos según sea necesario
+                    row.append(None)
             df_rows.append(row)
-        
+
         df = DataFrame(df_rows, columns=column_names)
-        
-        return df
+
+        return df, status
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error: {error_message}")
-        return DataFrame()  # Retorna un DataFrame vacío si la consulta falla
+        return DataFrame(), status  # Retorna un DataFrame vacío si la consulta falla
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-        return DataFrame()  # Retorna un DataFrame vacío si la consulta falla
-
-
+        return DataFrame(), status  # Retorna un DataFrame vacío si la consulta falla
 
 
 def query_to_dataframe(sql_query, cluster_identifier='redshift-data', database="landing_zone", region_name='us-west-2', db_user='admintreinta'):
     """
     Ejecuta una consulta SQL en Amazon Redshift y devuelve los resultados como un DataFrame de pandas.
-    
+
     Parámetros:
     - sql_query: Consulta SQL para ejecutar.
     - cluster_identifier: Identificador del cluster de Amazon Redshift.
     - database: Nombre de la base de datos.
     - db_user: Usuario de la base de datos.
-    
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
     client = boto3.client('redshift-data', region_name=region_name)
-    
+
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
         Database=database,
@@ -109,22 +111,23 @@ def query_to_dataframe(sql_query, cluster_identifier='redshift-data', database="
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
-    
+
     if status == 'FINISHED':
         response1 = client.get_statement_result(Id=statement_id)
-        
+
         # Extrayendo los nombres de las columnas de la metadata de columnas
         column_metadata = response1['ColumnMetadata']
         column_names = [column['name'] for column in column_metadata]
-        
+
         # Construyendo el DataFrame
         df = DataFrame([[
             field.get('stringValue') if 'stringValue' in field else
@@ -133,16 +136,17 @@ def query_to_dataframe(sql_query, cluster_identifier='redshift-data', database="
             field.get('booleanValue') if 'booleanValue' in field else
             None for field in record] for record in response1['Records']],
             columns=column_names)
-        
-        return df
+
+        return df, status
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error: {error_message}")
-        return DataFrame()  # Retorna un DataFrame vacío si la consulta falla
+        return DataFrame(), status  # Retorna un DataFrame vacío si la consulta falla
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-        return DataFrame()  # Retorna un DataFrame vacío si la consulta falla
+        return DataFrame(), status  # Retorna un DataFrame vacío si la consulta falla
 
 
 def dataframe_to_s3(df, bucket="redshift-python-datalake", endpoint='data_lake', region_name='us-west-2', object_name=''):
@@ -154,22 +158,23 @@ def dataframe_to_s3(df, bucket="redshift-python-datalake", endpoint='data_lake',
     # Generar un identificador único (puedes reemplazarlo por cualquier otra cadena aleatoria si prefieres)
     if object_name == '':
         object_name = uuid.uuid4().hex
-        
+
     object_path = f"{endpoint}/{year}/{month}/{day}/{object_name}_{timestamp}.csv.gz"
 
     # Usar BytesIO para datos binarios
     csv_buffer = BytesIO()
     df.to_csv(csv_buffer, index=False, compression='gzip')
-    
+
     # Es necesario mover el puntero del buffer al inicio después de escribir en él
     csv_buffer.seek(0)
 
     s3_resource = boto3.resource('s3', region_name=region_name)
     s3_resource.Object(bucket, object_path).put(Body=csv_buffer.getvalue())
-    
+
     return f's3://{bucket}/{object_path}'
 
-def load_s3_to_redshift(table,schema, s3_object_path, database='landing_zone', cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
+
+def load_s3_to_redshift(table, schema, s3_object_path, database='landing_zone', cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
     client = boto3.client('redshift-data', region_name=region_name)
     sql = f"""
         COPY {database}.{schema}.{table}
@@ -189,9 +194,9 @@ def load_s3_to_redshift(table,schema, s3_object_path, database='landing_zone', c
         DbUser=db_user,
         Sql=sql
     )
-    
+
     statement_id = response['Id']
-    
+
     # Espera a que la ejecución termine
     status = 'STARTED'
     while status in ['SUBMITTED', 'STARTED', 'PICKED']:
@@ -205,31 +210,32 @@ def load_s3_to_redshift(table,schema, s3_object_path, database='landing_zone', c
         print("La carga ha sido exitosa.")
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error al truncar la tabla: {error_message}")
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-    
-        
-    return response
 
-def execute_SP(store_procedure,schema,database = "landing_zone", cluster_identifier = 'redshift-data', db_user = 'admintreinta', region_name='us-west-2'):
+    return status
+
+
+def execute_SP(store_procedure, schema, database="landing_zone", cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
     """
     Ejecuta una un store en Amazon Redshift
-    
+
     Parámetros:
     - store_procedure: nombre del store procedure a ejecutar
     - schema: Esquema en el que se encuentra el store procedure.
     - database: Nombre de la base de datos.
     - cluster_identifier: Identificador del cluster de Amazon Redshift.
     - db_user: Usuario de la base de datos.
-    
-    
+
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
     client = boto3.client('redshift-data', region_name=region_name)
-    
+
     sql_query = f"CALL {database}.{schema}.{store_procedure}()"
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
@@ -239,28 +245,32 @@ def execute_SP(store_procedure,schema,database = "landing_zone", cluster_identif
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
     if status == 'FINISHED':
-        print ('Store Procedure ejecutado')
+        print('Store Procedure ejecutado')
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(error_message)
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-        return 0
 
-def truncate_table(table, schema, database = "landing_zone", cluster_identifier = 'redshift-data', db_user = 'admintreinta', region_name='us-west-2'):
+    return status
+
+
+def truncate_table(table, schema, database="landing_zone", cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
     """
     Ejecuta una un store en Amazon Redshift
-    
+
     Parámetros:
     - table: tabla a truncar
     - schema: Esquema en el que se encuentra el store procedure.
@@ -268,13 +278,13 @@ def truncate_table(table, schema, database = "landing_zone", cluster_identifier 
     - cluster_identifier: Identificador del cluster de Amazon Redshift.
     - database: Nombre de la base de datos.
     - db_user: Usuario de la base de datos.
-    
-    
+
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
     client = boto3.client('redshift-data', region_name=region_name)
-    
+
     sql_query = f"TRUNCATE {database}.{schema}.{table}"
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
@@ -284,44 +294,47 @@ def truncate_table(table, schema, database = "landing_zone", cluster_identifier 
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
-    
+
     if status == 'FINISHED':
-        print ('Store Procedure ejecutado')
+        print('Store Procedure ejecutado')
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error al truncar la tabla: {error_message}")
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-    return 0
 
-def drop_table(table, schema, database = "landing_zone", cluster_identifier = 'redshift-data', db_user = 'admintreinta', region_name='us-west-2'):
+    return status
+
+def drop_table(table, schema, database="landing_zone", cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
     """
     Ejecuta una un store en Amazon Redshift
-    
+
     Parámetros:
     - table: tabla a truncar
     - schema: Esquema en el que se encuentra el store procedure.
     - database: Nombre de la base de datos.
     - store_procedure: nombre del store procedure a ejecutar
     - cluster_identifier: Identificador del cluster de Amazon Redshift.
-    
+
     - db_user: Usuario de la base de datos.
-    
-    
+
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
     client = boto3.client('redshift-data', region_name=region_name)
-    
+
     sql_query = f"DROP TABLE {database}.{schema}.{table}"
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
@@ -331,41 +344,44 @@ def drop_table(table, schema, database = "landing_zone", cluster_identifier = 'r
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
-    
+
     if status == 'FINISHED':
-        print ('Taabla eliminada!')
+        print('Taabla eliminada!')
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error al eliminar la tabla: {error_message}")
     else:
         print("La operación fue abortada o no se completó exitosamente.")
-        return 0
 
-def sql_query(sql_query, database = "landing_zone",cluster_identifier = 'redshift-data', db_user = 'admintreinta', region_name='us-west-2'):
+    return status
+
+def sql_query(sql_query, database="landing_zone", cluster_identifier='redshift-data', db_user='admintreinta', region_name='us-west-2'):
     """
     Ejecuta una un store en Amazon Redshift
-    
+
     Parámetros:
     - sql_query: query a ejecutar en SQL
     - database: Nombre de la base de datos.
     - cluster_identifier: Identificador del cluster de Amazon Redshift.
     - db_user: Usuario de la base de datos.
-    
-    
+
+
     Retorna:
     - Un DataFrame de pandas con los resultados de la consulta.
     """
     client = boto3.client('redshift-data', region_name=region_name)
-    
+
     response = client.execute_statement(
         ClusterIdentifier=cluster_identifier,
         Database=database,
@@ -374,28 +390,33 @@ def sql_query(sql_query, database = "landing_zone",cluster_identifier = 'redshif
     )
 
     statement_id = response['Id']
-    
+
     # Espera hasta que la consulta se haya completado
     status = ''
     while status not in ['FINISHED', 'FAILED', 'ABORTED']:
-        time.sleep(5)  # Espera 5 segundos antes de verificar el estado nuevamente
+        # Espera 5 segundos antes de verificar el estado nuevamente
+        time.sleep(5)
         status_response = client.describe_statement(Id=statement_id)
         status = status_response['Status']
         print(f"Current status: {status}")
-    
+
     if status == 'FINISHED':
-        print ('Query ejecutada!')
+        print('Query ejecutada!')
     elif status == 'FAILED':
         # Obtiene y muestra el mensaje de error
-        error_message = status_response.get('Error', 'No se proporcionó información de error.')
+        error_message = status_response.get(
+            'Error', 'No se proporcionó información de error.')
         print(f"Error ejecutando la query SQL: {error_message}")
     else:
         print("La operación fue abortada o no se completó exitosamente.")
 
-        return 0
+    return status
+
 
 def dataframe_to_redshift(df, table, schema, bucket="redshift-python-datalake", database='landing_zone', region_name='us-west-2', endpoint='data_lake', object_name=None, db_user='admintreinta', cluster_identifier='redshift-data'):
     # Asegúrate de que todos los argumentos se pasen por nombre
-    s3_object_path = dataframe_to_s3(df=df, bucket=bucket, endpoint=endpoint, region_name=region_name, object_name=object_name)
-    output = load_s3_to_redshift(table=table, schema=schema, s3_object_path=s3_object_path, database=database, cluster_identifier=cluster_identifier, db_user=db_user, region_name=region_name)
+    s3_object_path = dataframe_to_s3(
+        df=df, bucket=bucket, endpoint=endpoint, region_name=region_name, object_name=object_name)
+    output = load_s3_to_redshift(table=table, schema=schema, s3_object_path=s3_object_path, database=database,
+                                 cluster_identifier=cluster_identifier, db_user=db_user, region_name=region_name)
     return output
